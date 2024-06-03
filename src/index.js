7aba4184770d85bbd672542f46b493c2804808d6
thoht3 <tho.hotrong@gmail.com>
2024-06-03 14:13:49 +0700

    Redone with webpack

---
+++ src/index.js
@@ -0,0 +1,34 @@
+let MAX = 57;
+let actual = MAX - 13;
+let percentage = actual/MAX;
+console.log(percentage);
+console.log("Hello, world!")
+
+let a = 5;
+let b = 7 * a;
+console.log(b)
+
+let add7 = (number) => number + 7;
+
+console.log(add7(7));
+
+let multi = (number1, number2) => number1 * number2;
+
+console.log(multi(2,3));
+
+let capitalize = (string1) => string1.toLowerCase().charAt(0).toUpperCase() + string1.toLowerCase().slice(1);
+console.log(capitalize("khong!!"));
+
+let lastLetter = (string1) => string1.charAt((string1.length - 1));
+
+console.log(lastLetter("dfasjfdfsjlk1!dfsfwss"));
+
+function add() {
+    return c;
+  }
+  
+  function print() {
+    add();
+  }
+  
+  print();
